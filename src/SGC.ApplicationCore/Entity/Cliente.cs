5510e69f215eeff3ce410d9b44e5c40616a1a35e
Fabio de Paula Silva <fabio.paula@systemsat.local>
2018-06-21 09:40:13 -0300

    Add project files.

---
+++ src/SGC.ApplicationCore/Entity/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SGC.ApplicationCore.Entity
+{
+    public class Cliente
+    {
+        //construtor
+        public Cliente()
+        {
+
+        }
+
+        //propriedades
+        public int ClienteId { get; private set; }
+
+        public string Nome { get; private set; }
+
+        public string CPF { get; private set; }
+        
+    }
+}
